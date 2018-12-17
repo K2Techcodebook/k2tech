c68dafd48294bcfbe87fc5be667901ae47685f08
kennyendowed <kenneyg50@gmail.com>
2018-12-17 10:40:54 +0100

    today

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('isadmin')->nullable();
+            $table->string('ip_address');
             $table->rememberToken();
             $table->timestamps();
         });
